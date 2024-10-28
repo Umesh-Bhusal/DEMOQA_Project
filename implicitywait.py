230830fe4437ac9c1947b2f14b36c5d380556649
umesh Bhusal <anishbhusal30@gmail.com>
2024-10-28 15:38:34 +0545

    added new code from google to site redirection

---
+++ implicitywait.py
@@ -0,0 +1,32 @@
+#Run this cmd   pip install selenium webdriver-manager first
+from select import select
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from trio import current_time
+import time
+from selenium.webdriver.common.devtools.v127.fed_cm import click_dialog_button
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+# Initialize the ChromeDriver service
+service = Service(ChromeDriverManager().install())
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome(service=service)
+
+# Navigate to a specific URL
+website_url=driver.get("https://www.google.com/")  # Replace with your desired URL
+driver.maximize_window()
+
+searchbox= driver.find_element(By.NAME, "q")
+searchbox.send_keys("Selenium")
+searchbox.submit()
+time.sleep(3)
+
+sele= driver.find_element(By.XPATH, "//a[@href='https://www.selenium.dev/']//h3[@class='LC20lb MBeuO DKV0Md'][normalize-space()='Selenium']")
+sele.click()
+time.sleep(4)
--- sharesanser/test.py
+++ sharesanser/test.py
@@ -7,18 +7,52 @@ import time
 
 # Set up the Chrome WebDriver
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-website_url = "https://www.sharesansar.com/"
-driver.get(website_url)
-time.sleep(3)
 
-# Maximize the window
+driver.get("https://www.google.com/")
 driver.maximize_window()
-time.sleep(3)
 
-# Enter "NABIL" in the search field and press Enter
-search_box = driver.find_element(By.XPATH, "/html/body/div[2]/div/header/div[1]/div/div[1]/div/div/div[2]/form/div/div/div[1]/input")
-search_box.send_keys("NABIL", Keys.RETURN)
-# Optional delay to observe results before quitting
-time.sleep(5)
+share= driver.find_element(By.NAME, "q")
+share.send_keys("sharesansar")
+share.submit()
+
+submit_share= driver.find_element(By.XPATH, "//a[@href='https://www.sharesansar.com/']//h3[@class='LC20lb MBeuO DKV0Md'][normalize-space()='ShareSansar']")
+submit_share.click()
+
+insidesite = driver.find_element(By.XPATH,"//input[@id='company_search']")
+insidesite.send_keys("Nabil")
+insidesite.submit()
+time.sleep(4)
+
+
+
+# #######################################################################
+#You can use both option  down or above both code work fine
+##########################################################################
+
+
+
+
+
+
+
+
+# website_url = "https://www.sharesansar.com/"
+# driver.get(website_url)
+# time.sleep(3)
+#
+# # Maximize the window
+# driver.maximize_window()
+# time.sleep(3)
+
+# # Enter "NABIL" in the search field and press Enter
+# search_box = driver.find_element(By.XPATH, "/html/body/div[2]/div/header/div[1]/div/div[1]/div/div/div[2]/form/div/div/div[1]/input")
+# search_box.send_keys("NABIL", Keys.RETURN)
+# # Optional delay to observe results before quitting
+# time.sleep(5)
 
 # Quit the driver
+
+
+
+
+
